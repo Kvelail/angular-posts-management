d82ef0bb2666da0aef7f6fd5a03ebadbbebd4c0f
Kvelail <talikadic@gmail.com>
2023-07-15 15:00:11 +0200

    remove the excess

--- src/app/components/innovic-footer/innovic-footer.component.ts
+++ src/app/components/innovic-footer/innovic-footer.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { NAVBAR_MENU_ITEMS } from 'src/app/state/utils/static';
 
-import { OnlyTitleModel } from 'src/app/state/model/only-title.model';
-
 @Component({
     selector: 'app-innovic-footer',
     templateUrl: './innovic-footer.component.html',
@@ -12,17 +10,13 @@ import { OnlyTitleModel } from 'src/app/state/model/only-title.model';
 export class InnovicFooterComponent {
     public copyrightYear: number | null = null;
 
-    public navbarMenuItems: OnlyTitleModel[] = [];
-
     ngOnInit(): void {
-        this.getNavbarMenuItemsAndFullYear();
+        this.getFullYear();
     }
 
     public trackByIdentity = (index: number, _: any): number => index;
 
-    private getNavbarMenuItemsAndFullYear(): void {
-        this.navbarMenuItems = NAVBAR_MENU_ITEMS;
-
+    private getFullYear(): void {
         this.copyrightYear = new Date().getFullYear();
     }
 }
